893394c1ef2971577d7625d7d2af6f8039392030
leonbeltramazzaferri <leonbeltranm07@gmail.com>
2025-08-29 22:04:35 -0300

    intentando que el mapa se actualice en tiempo real

--- avellanedaUnida/componentes/mapa.js
+++ avellanedaUnida/componentes/mapa.js
@@ -1,57 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 
 export default function Mapa() {
-  const [location, setLocation] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        return;
-      }
-
-      
-      const subscription = await Location.watchPositionAsync(
-        { accuracy: Location.Accuracy.High, timeInterval: 1000, distanceInterval: 10 },
-        (loc) => setLocation(loc.coords)
-      );
-
-      return () => subscription.remove();
-    })();
-  }, []);
-
-  const region = location
-    ? {
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }
-    : {
-        latitude: -34.6621,
-        longitude: -58.3646,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      };
+  const avellaneda = {
+    latitude: -34.6621,
+    longitude: -58.3646,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
 
   return (
     <View style={styles.container}>
       <MapView
-        provider={MapView.PROVIDER_GOOGLE}
         style={styles.map}
-        region={region}
+        region={avellaneda}
         minZoomLevel={12}
         maxZoomLevel={18}
       >
         <Marker
-          coordinate={{
-            latitude: region.latitude,
-            longitude: region.longitude,
-          }}
-          title={location ? "Tu ubicación" : "¡Estás en Avellaneda!"}
+          coordinate={{ latitude: -34.6621, longitude: -58.3646 }}
+          title={"¡Estás en Avellaneda!"}
         />
       </MapView>
     </View>
