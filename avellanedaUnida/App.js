bf2544a0e36d3e5925f731d6ac955bbad96a2284
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-10-24 20:41:53 -0300

    voy a quedarme pelado a los 20

--- avellanedaUnida/App.js
+++ avellanedaUnida/App.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-// Componentes
-import Inicio from "./components/Inicio";
-import Mapa from "./components/mapa";
-import PrimerComponente from "./components/PrimerComponente";
-import TercerComponente from "./components/TercerComponente";
+import Inicio from './components/Inicio';
+import Noticias from './components/Noticias';
+import Mapa from './components/mapa';
 
 const Tabs = createBottomTabNavigator();
 
@@ -16,41 +14,18 @@ export default function App() {
     <NavigationContainer>
       <Tabs.Navigator
         screenOptions={({ route }) => ({
-          headerShown: false,
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === "Bandera") {
-              iconName = focused ? "flag" : "flag-outline";
-            } else if (route.name === "Inicio") {
-              iconName = focused ? "home" : "home-outline";
-            } else if (route.name === "Mapas") {
-              iconName = focused ? "map" : "map-outline";
-            } else if (route.name === "Noticias") {
-              iconName = focused ? "newspaper" : "newspaper-outline";
-            }
+            if (route.name === 'Inicio') iconName = focused ? 'home' : 'home-outline';
+            else if (route.name === 'Mapas') iconName = focused ? 'map' : 'map-outline';
+            else if (route.name === 'Noticias') iconName = focused ? 'newspaper' : 'newspaper-outline';
             return <Ionicons name={iconName} size={size} color={color} />;
-          },
-
-          tabBarActiveTintColor: "#007AFF",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: {
-            backgroundColor: "#ffffff",
-            borderTopWidth: 1,
-            borderTopColor: "#e0e0e0",
-            height: 60,
-            paddingBottom: 5,
-          },
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "600",
-            marginBottom: 3,
           },
         })}
       >
         <Tabs.Screen name="Inicio" component={Inicio} />
         <Tabs.Screen name="Mapas" component={Mapa} />
-        <Tabs.Screen name="Bandera" component={PrimerComponente} />
-        <Tabs.Screen name="Noticias" component={TercerComponente} />
+        <Tabs.Screen name="Noticias" component={Noticias} />
       </Tabs.Navigator>
     </NavigationContainer>
   );
